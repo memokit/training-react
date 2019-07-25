485a9e977c1eded72e67896dd99a75cce4954b91
Kit <kit.jrs@gmail.com>
2019-07-25 10:31:33 +0700

    Update function multiply

--- calculator-kit/index.js
+++ calculator-kit/index.js
@@ -1,5 +1,7 @@
 module.exports = {
     plus: (first,second ) => first + second,
 
-    subtract: (first,second ) => first - second
+    subtract: (first,second ) => first - second,
+
+    multiply: (first, second) => first * second
 }
--- calculator-kit/test.js
+++ calculator-kit/test.js
@@ -2,4 +2,6 @@ const cal = require('./index');
 
 console.log(cal.plus(3,2));
 console.log(cal.subtract(5,3));
+console.log(cal.multiply(6, 3));
+
 
